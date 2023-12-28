d2838b73d1c8c713d588d90d4a280480c2e7e89c
guchu-gacheru <guchugacheru@gmail.com>
2023-12-29 02:05:45 +0300

    .

--- area.py
+++ area.py
@@ -1,8 +1,16 @@
-#Area of a Rectangle
+# Area of a Rectangle
 
 length = int(input("Enter the longest size indicated: "))
 width = int(input("Enter the shortest size indicated: "))
 
 area = length * width
 
-print("Area of the rectangle is", area,".")
+print("Area of the rectangle is", area, ".")
+
+
+# Area of a rectangle as a function
+def area():
+    return (length * width)
+
+
+print("Area of the rectangle is", area(), ".")
